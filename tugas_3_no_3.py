fc0987979ea67c24997a9c873da9527e7d4cd311
Suhro-st <suhrow62@gmail.com>
2024-10-21 14:58:05 +0800

    Update tugas_3_no_3.py

--- tugas_3_no_3.py
+++ tugas_3_no_3.py
@@ -1,9 +1,6 @@
-n = 6 
-for i in range(1, n+1):
-    print('  ' * (n - i), end='')
-    for j in range(1, i+1):
-        if j == 1:
-            print("*", end='')
-        else:
-            print(" . *", end='')
-    print()
+print(" " * 7 + "*")
+for i in range(1, 7 + 1):
+    print(" " * (7 - i), end="")
+    for j in range(i):
+        print("*", end=".")
+    print("*")
