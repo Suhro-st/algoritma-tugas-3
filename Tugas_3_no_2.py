6f64e7a0a846a61a196f079a2855b73feba735cc
Suhro-st <suhrow62@gmail.com>
2024-10-21 14:42:39 +0800

    Update Tugas_3_no_2.py

--- Tugas_3_no_2.py
+++ Tugas_3_no_2.py
@@ -1,6 +1,4 @@
-def print_pattern(n):
-    for i in range(n):
-        print(" " * (i * 2) + "* " * (n - i))
-
-n = 8
-print_pattern(n)
+for i in range(7 ,0,-1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print() 
